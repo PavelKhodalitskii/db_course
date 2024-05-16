0df397f55381391e75d087757c7b9348bce4c5b8
Pavel <dreamsobenatic00@mail.ru>
2024-05-16 19:55:12 +0800

    Indexes fix

--- indexes.sql
+++ indexes.sql
@@ -1,18 +1,18 @@
 -- Индекс для таблицы видео. Часто нам нужны самые новые видео
-create index idx_videos_by_time_idx on Videos (ValidFrom)
+create index idx_videos_by_time_idx on Videos (ValidFrom);
 
 -- Аналогично с комментариями
-create index idx_comments_idx on Comments (CreationData)
+create index idx_comments_idx on Comments (CreationData);
 
 -- Часто поиск видео происходит именно по заголовку - этот индекс должен оптимизировать поиск
-create index idx_videos_titles_idx on Videos (Title)
+create index idx_videos_titles_idx on Videos (Title);
 
 -- Аналогично с плейлистами
-create index idx_playlists_titles_idx on Playlists (Title)
+create index idx_playlists_titles_idx on Playlists (Title);
 
 -- Нас часто интересуют подписки на какого-то из пользователя
 
-create index idx_subscribers_idx on Subscriptions(Subscriber)
+create index idx_subscribers_idx on Subscriptions(Subscriber);
 
 -- Так же и в обратную сторону
-create index idx_subscribed_on_idx on Subscriptions(SubscribedTo)
+create index idx_subscribed_on_idx on Subscriptions(SubscribedTo);
